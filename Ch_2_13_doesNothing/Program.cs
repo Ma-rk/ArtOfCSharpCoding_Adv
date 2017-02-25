47876bfb51371310ac2ce233e88e95967893b672
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-02-25 16:38:11 +0900

    Ch-2-13: doesNothing

---
+++ Ch_2_13_doesNothing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ch_2_13_doesNothing
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			IReportBase[] reportAry = { new NoReport()
+									  , new ConsoleReport() };
+
+			foreach (var report in reportAry)
+			{
+				report.Outout("Hello World!");
+			}
+		}
+	}
+
+	class NoReport : IReportBase
+	{
+		public void Outout(string s)
+		{
+		}
+	}
+
+	class ConsoleReport : IReportBase
+	{
+		public void Outout(string s)
+		{
+			Console.WriteLine(s);
+		}
+	}
+
+	interface IReportBase
+	{
+		void Outout(string s);
+	}
+}
