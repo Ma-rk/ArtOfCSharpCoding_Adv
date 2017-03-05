48a4e1adbb6e405be74fb0948bafb6bcf66f9817
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-03-05 23:41:08 +0900

    Ch-3-04: array, IEnumerableT 2

--- Ch_3_04_array_IEnumerableT/Program.cs
+++ Ch_3_04_array_IEnumerableT/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Ch_3_04_array_IEnumerableT
 {
 	class MainClass
 	{
+		const int SIZE = 1000000;
+		const int COUNT = 100000000;
+		static int n = 0;
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] ary = new int[SIZE];
+			ary[SIZE - 1] = 987;
+
+			Stopwatch sw = new Stopwatch();
+
+			sw.Start();
+
+			for (int i = 0; i < COUNT; i++)
+				n = ary.ElementAtOrDefault(SIZE - 1);
+
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+
+			sw.Reset();
+			sw.Start();
+
+			for (int i = 0; i < COUNT; i++)
+				n = ary[SIZE - 1];
+
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+
+			sw.Reset();
+			sw.Start();
+
+			var q = EnumSample();
+			var r1 = q.First();
+
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+
+
+			sw.Reset();
+			sw.Start();
+
+			var ar = EnumSample().ToArray();
+			var r2 = ar[0];
+
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+		}
+
+		private static IEnumerable<int> EnumSample()
+		{
+			yield return 1;
+			yield return 2;
 		}
 	}
 }
